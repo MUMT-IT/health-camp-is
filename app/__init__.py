f4fec9bdac2ff5ab1a28a3b7969a3f8a25130814
Likit Preeyanon <likit.pre@mahidol.edu>
2023-03-13 16:10:02 +0700

    fixed postgresql error again

--- app/__init__.py
+++ app/__init__.py
@@ -19,10 +19,8 @@ def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     database_url = os.environ.get('DATABASE_URL')
-    if database_url.startswith('postgresql'):
-        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+    if not database_url.startswith('postgresql'):
+        database_url = database_url.replace('postgres', 'postgresql')
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY'),
         SQLALCHEMY_DATABASE_URI=database_url,
